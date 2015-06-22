226a1d0c6217cfaed66e9414a2f88f1dd30081f8
Marcin Nowicki <pr0d1r2@gmail.com>
2015-06-22 10:50:05 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,32 +16,32 @@ ActiveRecord::Schema.define(version: 20141125110523) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "bad_words", force: true do |t|
-    t.string   "word",       null: false
+  create_table "bad_words", force: :cascade do |t|
+    t.string   "word",       limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "bad_words", ["word"], name: "index_bad_words_on_word", unique: true, using: :btree
 
-  create_table "failed_videos", force: true do |t|
+  create_table "failed_videos", force: :cascade do |t|
     t.string   "md5",        limit: 32, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "keywords", force: true do |t|
-    t.string   "keyword",    null: false
-    t.string   "categories", null: false
+  create_table "keywords", force: :cascade do |t|
+    t.string   "keyword",    limit: 255, null: false
+    t.string   "categories", limit: 255, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "magnet_sources", force: true do |t|
-    t.string   "magnet_keyword",               null: false
-    t.integer  "category",                     null: false
-    t.integer  "sort_by",         default: 7,  null: false
-    t.integer  "number_of_pages", default: 10, null: false
+  create_table "magnet_sources", force: :cascade do |t|
+    t.string   "magnet_keyword",  limit: 255,              null: false
+    t.integer  "category",                                 null: false
+    t.integer  "sort_by",                     default: 7,  null: false
+    t.integer  "number_of_pages",             default: 10, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "keyword_id"
@@ -50,19 +50,19 @@ ActiveRecord::Schema.define(version: 20141125110523) do
   add_index "magnet_sources", ["keyword_id"], name: "index_magnet_sources_on_keyword_id", using: :btree
   add_index "magnet_sources", ["magnet_keyword", "category"], name: "index_magnet_sources_on_magnet_keyword_and_category", unique: true, using: :btree
 
-  create_table "magnets", force: true do |t|
-    t.text     "link",                                   null: false
+  create_table "magnets", force: :cascade do |t|
+    t.text     "link",                                     null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "title",                                  null: false
-    t.integer  "seeders",                                null: false
-    t.integer  "leechers",                               null: false
-    t.string   "category",                               null: false
-    t.integer  "torrent_id",                             null: false
-    t.string   "url",                                    null: false
-    t.integer  "magnet_source_id",                       null: false
-    t.integer  "flags",                      default: 0, null: false
-    t.string   "workflow_state"
+    t.string   "title",            limit: 255,             null: false
+    t.integer  "seeders",                                  null: false
+    t.integer  "leechers",                                 null: false
+    t.string   "category",         limit: 255,             null: false
+    t.integer  "torrent_id",                               null: false
+    t.string   "url",              limit: 255,             null: false
+    t.integer  "magnet_source_id",                         null: false
+    t.integer  "flags",                        default: 0, null: false
+    t.string   "workflow_state",   limit: 255
     t.integer  "files"
     t.integer  "size",             limit: 8
     t.datetime "uploaded"
@@ -72,19 +72,19 @@ ActiveRecord::Schema.define(version: 20141125110523) do
   add_index "magnets", ["magnet_source_id"], name: "index_magnets_on_magnet_source_id", using: :btree
   add_index "magnets", ["torrent_id"], name: "index_magnets_on_torrent_id", unique: true, using: :btree
 
-  create_table "videos", force: true do |t|
-    t.string   "name",                                         null: false
+  create_table "videos", force: :cascade do |t|
+    t.string   "name",                  limit: 255,             null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "video_file_name"
-    t.string   "video_content_type"
+    t.string   "video_file_name",       limit: 255
+    t.string   "video_content_type",    limit: 255
     t.integer  "video_file_size",       limit: 8
     t.datetime "video_updated_at"
-    t.string   "duration",                                     null: false
-    t.integer  "flags",                            default: 0, null: false
+    t.string   "duration",              limit: 255,             null: false
+    t.integer  "flags",                             default: 0, null: false
     t.string   "original_md5_checksum", limit: 32
     t.datetime "deleted_at"
-    t.string   "videoable_type"
+    t.string   "videoable_type",        limit: 255
     t.integer  "videoable_id"
   end
 
